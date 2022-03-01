c971a2840ea8d80a159cbb77b4924d7e112d5c26
Sanzhar Alimkhanov <alimkhanovsanzhar@gmail.com>
2022-03-01 14:44:23 +0600

    .

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import vue              from '@vitejs/plugin-vue';
+import path             from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [ vue() ],
+  resolve: {
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname,
+    },
+  },
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'ZYXLib',
+      fileName: (format) => `zyx-lib.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [ 'vue' ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
